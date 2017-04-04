20543ddeb9126c8628dda7d710bfb1a79bcb5e3b
MCess <m.cessaro@gmail.com>
2017-04-04 13:25:09 -0500

    devise installed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
